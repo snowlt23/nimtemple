6f960b764826469f4263aab9ddfc59ecfd81fece
snowlt23 <shsnow23@gmail.com>
2017-05-26 00:00:55 +0900

    add extends syntax, add value syntax

--- nimtemple.nim
+++ nimtemple.nim
@@ -6,8 +6,16 @@ export json
 let includePattern* = peg"""
 "{{" \s* "include" \s+ "\"" {@} "\"" \s* "}}"
 """
+let contentPattern* = peg"""
+"{{" \s* "content" \s* "}}"
+"""
+let extendsPattern* = peg"""
+"{{" \s* "extends" \s+ "\"" {@} "\"" \s* "}}"
+"""
 let forPattern* = peg"""
-"{{" \s* "for" \s+ {\ident} \s+ "in" \s+ {\ident} \s* "}}" {@} ("{{" \s* "end" \s* "}}")
+for <- "{{" \s* "for" \s+ {\ident} \s+ "in" \s+ val \s* "}}" {(for / (!end .))*} end
+val <- {\ident} ("." {\ident})*
+end <- ("{{" \s* "end" \s* "}}")
 """
 let valPattern* = peg"""
 "${" \s* {\ident} ("." {\ident})*  \s* "}"
@@ -23,28 +31,31 @@ proc `[]`*(tmpl: TemplateRenderer, key: string): JsonNode =
   tmpl.obj[key]
 proc `[]=`*(tmpl: TemplateRenderer, key: string, val: JsonNode) =
   tmpl.obj[key] = val
+proc getVal*(tmpl: TemplateRenderer, keys: openArray[string]): JsonNode =
+  var val = tmpl[keys[0]]
+  for key in @keys[1..^1]:
+    val = val[key]
+  return val
 proc render*(tmpl: TemplateRenderer, src: string): string =
   result = src
+  var extendsmatches = newSeq[string](1)
+  if result.find(extendsPattern, extendsmatches) != -1:
+    result = readFile(extendsmatches[0]).replace(contentPattern, result.replace(extendsPattern, ""))
   result = result.replace(includePattern) do (m, n: int, c: openArray[string]) -> string:
+    # include
     let filename = c[0]
     result = tmpl.render(readFile(filename))
   result = result.replace(forPattern) do (m, n: int, c: openArray[string]) -> string:
+    # for
     result = ""
     let
       elemname = c[0]
-      itername = c[1]
-      content = c[2]
-    for elem in tmpl[itername]:
-      result &= content.replace(valPattern) do (m, n: int, c: openArray[string]) -> string:
-        var val = if elemname == c[0]:
-                    elem
-                  else:
-                    tmpl[c[0]]
-        for i in 1..<n:
-          val = val[c[i]]
-        return val.str
+      itername = @c[1..n-2]
+      content = c[n-1]
+    for elem in tmpl.getVal(itername):
+      var curtmpl = tmpl
+      curtmpl[elemname] = elem
+      result &= curtmpl.render(content)
   result = result.replace(valPattern) do (m, n: int, c: openArray[string]) -> string:
-    var val = tmpl[c[0]]
-    for i in 1..<n:
-      val = val[c[i]]
-    return val.str
+    # val
+    tmpl.getVal(@c[0..<n]).str

4f6d75a197a07e11212f507236d6dcde17708e7d
snowlt23 <shsnow23@gmail.com>
2017-05-26 03:22:33 +0900

    adda if statement

--- nimtemple.nim
+++ nimtemple.nim
@@ -12,8 +12,11 @@ let contentPattern* = peg"""
 let extendsPattern* = peg"""
 "{{" \s* "extends" \s+ "\"" {@} "\"" \s* "}}"
 """
-let forPattern* = peg"""
-for <- "{{" \s* "for" \s+ {\ident} \s+ "in" \s+ val \s* "}}" {(for / (!end .))*} end
+let statementPattern* = peg"""
+statement <- for / if
+for <- "{{" \s* {"for"} \s+ {\ident} \s+ "in" \s+ val \s* "}}" {(!statement / (!end .))*} end
+if <- "{{" \s* {"if"} \s+ val \s* "}}" {(!statement / (!end !else .))*} else? end
+else <- "{{" \s* {"else"} \s* "}}" {(!statement / (!end .))*}
 val <- {\ident} ("." {\ident})*
 end <- ("{{" \s* "end" \s* "}}")
 """
@@ -40,22 +43,44 @@ proc render*(tmpl: TemplateRenderer, src: string): string =
   result = src
   var extendsmatches = newSeq[string](1)
   if result.find(extendsPattern, extendsmatches) != -1:
+    # extends
     result = readFile(extendsmatches[0]).replace(contentPattern, result.replace(extendsPattern, ""))
   result = result.replace(includePattern) do (m, n: int, c: openArray[string]) -> string:
     # include
     let filename = c[0]
     result = tmpl.render(readFile(filename))
-  result = result.replace(forPattern) do (m, n: int, c: openArray[string]) -> string:
+  result = result.replace(statementPattern) do (m, n: int, c: openArray[string]) -> string:
     # for
     result = ""
-    let
-      elemname = c[0]
-      itername = @c[1..n-2]
-      content = c[n-1]
-    for elem in tmpl.getVal(itername):
-      var curtmpl = tmpl
-      curtmpl[elemname] = elem
-      result &= curtmpl.render(content)
+    echo @c
+    case c[0]
+    of "for":
+      let
+        elemname = c[1]
+        itername = @c[2..n-2]
+        content = c[n-1]
+      for elem in tmpl.getVal(itername):
+        var curtmpl = tmpl
+        curtmpl[elemname] = elem
+        result &= curtmpl.render(content)
+    of "if":
+      if c[n-2] == "else":
+        let
+          value = tmpl.getVal(@c[1..n-4])
+          tcontent = c[n-3]
+          fcontent = c[n-1]
+        if value.bval:
+          result &= tmpl.render(tcontent)
+        else:
+          result &= tmpl.render(fcontent)
+      else:
+        let
+          value = tmpl.getVal(@c[1..n-3])
+          tcontent = c[n-2]
+        if value.bval:
+          result &= tmpl.render(tcontent)
+    else:
+      discard
   result = result.replace(valPattern) do (m, n: int, c: openArray[string]) -> string:
     # val
     tmpl.getVal(@c[0..<n]).str
